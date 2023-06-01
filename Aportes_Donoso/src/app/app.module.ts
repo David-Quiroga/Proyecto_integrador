40cef5b3cce4c9417124ef806405eb07b2dd6687
David Quiroga <97131802+David-Quiroga@users.noreply.github.com>
2023-05-31 21:30:54 -0500

    index

--- Aportes_Donoso/src/app/app.module.ts
+++ Aportes_Donoso/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { IndexLoginComponent } from './pages/index-login/index-login.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IndexLoginComponent
+    IndexLoginComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
